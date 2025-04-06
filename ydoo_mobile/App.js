8e9efeb84b21ac579f1489c86a7b983e3801ef2d
maroua sedoud <77921843+marouasedoud@users.noreply.github.com>
2025-04-06 11:33:28 +0100

    initialize react native expo app

---
+++ ydoo_mobile/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { useState } from 'react';
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <View style={styles.container}>
+      <Text>Hello world</Text>
+      <Button onPress={() => setCount(count + 1)} title='click me'/>
+      <Text>You clicked {count} times</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
